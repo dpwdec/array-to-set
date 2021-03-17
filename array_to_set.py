4d9fba654e89c46c387c2a01823506ed95d80c97
Dec Kolakowski <declankolakowski@gmail.com>
2021-03-17 12:41:38 +0000

    Finish array to set

--- array_to_set.py
+++ array_to_set.py
@@ -1,39 +1,16 @@
-
-
 def array_to_set(nums: list) -> list:
+    if len(nums) <= 2: return nums
+
     # destructure array
     x, *xs = nums
 
-    ys = simple_split(xs)
+    # split array into two sections on different elements
+    ys = [y for y in xs 
+            if y == x]
 
-    zs = xs[:len(ys)]
+    zs = xs[len(ys):]
 
-    return [x + array_to_set(zs + ys)]
-
-def split_array(a: list):
-    x, y, *xs = a
-    if x == y:
-        return [x] + split_array([y] + xs)
-    else:
-        return [x]
-
-def split_array_2(a: list):
-    goodvals = [1]
-    good, bad = [], []
-    for x in a:
-        (bad, good)[x in goodvals].append(x)
-    return [good, bad]
-
-def split_array_3(a: list):
-    x, *xs = a
-    return [[y for y in a if y in [x]], [y for y in a if not y in [x]]]
-
-# g = [1, 1, 2, 3, 4, 5, 6]
-
-# gs = split_array(g)
-
-# hs = g[len(gs):]
-
-# print([gs, hs])
-
-print(split_array_2([1, 1, 1, 2, 3, 4]))
+    # switch array by
+    # putting elements of array that match x to end
+    # recursive call
+    return [x] + array_to_set(zs + ys)
--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from array_to_set import array_to_set
 
-@pytest.mark.parametrize([1, 2, 3, 4])
-def test_array(input):
-    assert array_to_set(input) == 1
+def test_array_singleton():
+    assert array_to_set([1]) == [1]
+
+def test_array_1d():
+    assert array_to_set([1, 1, 1]) == [1, 1, 1]
+
+def test_array_long():
+    assert array_to_set([1, 1, 1, 2, 2, 3, 4]) == [1, 2, 3, 4, 1, 2, 1]
